10913c7ff71bdb6fe600a321b1f3afdb36f76fe9
Andrew <s0lbjorandr3w@gmail.com>
2026-06-06 18:22:35 -0700

    adding comments

--- Unit2/Session1.py
+++ Unit2/Session1.py
@@ -1,19 +1,26 @@
 # Problem 3: Ticket Sales
 ### U - Understand 
 # 1. Share 2 questions you would ask to help understand the question:
-# 
+# What kind of data structure is ticket sales?
+# Can we access only the values of a dictionary?
 
 ### P - Plan
 # 2. Write out in plain English what you want to do: 
-# 
+# I want to declare a variable to hold the total. Then I want to loop through
+# the values of the dictionary, and add each value to the total. Last I want to
+# return the total.
 
 # 3. Translate each sub-problem into pseudocode:
-# 
+# total = 0
+# for value in dictionary.values():
+#   add value to total
+# return total
 
 ### I - Implement
 # 4. Translate the pseudocode into Python and share your final answer:
 
-# 
+# I chose this problem because it handles some of the basics of what a dictionary is. There's also a 
+# way to solve it with using .values() which is a useful function to know.  
 
 def total_sales(ticket_sales):
     total = 0
@@ -27,19 +34,29 @@ print(total_sales(ticket_sales))
 # Problem 4: Scheduling Conflict
 ### U - Understand 
 # 1. Share 2 questions you would ask to help understand the question:
-# 
+# Can I access the key and value of a dictionary in a single loop statement?
+# How do I check if key-values are the same in both dicitionaries?
 
 ### P - Plan
 # 2. Write out in plain English what you want to do: 
-# 
+# I want to declare a variable to hold the conflicts. Then I want to loop 
+# through the first dictionary, and for each key-value pair, check if the 
+# key exists in the second dictionary and if the value is the same. 
+# If both conditions are true, I want to add that key-value pair to the conflicts variable. 
+# Last I want to return the conflicts.
 
 # 3. Translate each sub-problem into pseudocode:
-# 
+# conflicts = {}
+# for key, value in dictionary1.items():
+#     if key in dictionary2 and value == dictionary2[key]:
+#         add key-value pair to conflicts
+# return conflicts
 
 ### I - Implement
 # 4. Translate the pseudocode into Python and share your final answer:
 
-# 
+# I chose this problem because it shows us how to loop through a dictionary and access both
+# the key and value at the same time. It also allows for checking the existence of a key in another dictionary.
 
 def identify_conflicts(venue1_schedule, venue2_schedule):
     conflicts = {}
@@ -67,19 +84,31 @@ print(identify_conflicts(venue1_schedule, venue2_schedule))
 # Problem 10: VIP Passes and Guests
 ### U - Understand 
 # 1. Share 2 questions you would ask to help understand the question:
-# 
+# Should I treat uppercase and lowercase characters as the same VIP pass?
+# How do I declare a set in Python and check if a character is in that set?
 
 ### P - Plan
 # 2. Write out in plain English what you want to do: 
-# 
+# I want to first create a set called vip_set, and loop through the characters in 
+# vip_passes adding each character to the set. Afterwards I set a count variable = 0, then loop over the
+# characters in guests, and if the character is in vip_set, I increment the count, returning the final count.
 
 # 3. Translate each sub-problem into pseudocode:
-# 
+# vip_set = set()
+# for char in list1:
+#   vip_set.add(char)
+# count = 0
+# for char in list2:
+#   if char in vip_set:
+#       add to count
+# return count
 
 ### I - Implement
 # 4. Translate the pseudocode into Python and share your final answer:
 
-# 
+# I chose this problem because it introduces sets, which are a new structure people may not be
+# familiar. They also have their own set of notation that needs to be used so its good to learn
+# how to for instance add or remove from them. 
 
 def num_VIP_guests(vip_passes, guests):
     vip_set = set()
